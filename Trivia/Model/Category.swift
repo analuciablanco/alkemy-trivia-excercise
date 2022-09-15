cbacf505e3cb1290c1c2d9407e8c0c77ec9f4ee6
Ana Lucia Blanco <analuciablanco@outlook.com>
2022-09-15 12:10:45 -0700

    changes

---
+++ Trivia/Model/Category.swift
@@ -0,0 +1,8 @@
+//
+//  Category.swift
+//  Trivia
+//
+//  Created by Ana Lucia Blanco Cervantes on 13/09/22.
+//
+
+import Foundation
---
+++ Trivia/TabBarController.swift
@@ -0,0 +1,29 @@
+//
+//  TabBarController.swift
+//  Trivia
+//
+//  Created by Ana Lucia Blanco Cervantes on 13/09/22.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
